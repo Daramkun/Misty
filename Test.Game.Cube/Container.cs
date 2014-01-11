7a1da1099351bc39da3c33f77c3ace7de1ea2c34
Jin Jae-yeon <daramkun@daram.pe.kr>
2014-01-11 20:56:29 +0900

    Add SXML schema + Add Cube render test project

---
+++ Test.Game.Cube/Container.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using Daramkun.Misty.Common;
+using Daramkun.Misty.Graphics;
+using Daramkun.Misty.Mathematics;
+using Daramkun.Misty.Nodes;
+
+namespace Test.Game.Cube
+{
+	[MainNode]
+    public class Container : Node
+    {
+		[StructLayout ( LayoutKind.Sequential )]
+		private struct CubeVertex
+		{
+			[VertexElementation ( ElementType.Position )]
+			public Vector3 Position;
+			[VertexElementation ( ElementType.Diffuse )]
+			public Color Diffuse;
+		}
+
+		IVertexBuffer cubeVertices;
+		IVertexDeclaration vertexDeclarataion;
+
+		public override void Intro ( params object [] args )
+		{
+
+			base.Intro ( args );
+		}
+
+		public override void Outro ()
+		{
+			base.Outro ();
+		}
+
+		public override void Update ( GameTime gameTime )
+		{
+			base.Update ( gameTime );
+		}
+
+		public override void Draw ( GameTime gameTime )
+		{
+			base.Draw ( gameTime );
+		}
+    }
+}
