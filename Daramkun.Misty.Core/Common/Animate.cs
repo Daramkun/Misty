b5242197f3f1c1d52c73e12fbf2f1174eab02f25
Jin Jae-yeon <daramkun@daram.pe.kr>
2014-02-01 23:36:56 +0900

    add Animate class

---
+++ Daramkun.Misty.Core/Common/Animate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Daramkun.Misty.Common
+{
+	public class Animate
+	{
+		TimeSpan lastElapsed;
+
+		public TimeSpan Position { get; private set; }
+		public TimeSpan Duration { get; set; }
+		public double Objective { get; set; }
+		public double Animated { get; private set; }
+		public double TotalAnimated { get; private set; }
+		public bool IsAnimating { get; private set; }
+
+		public Animate () { Stop (); }
+		public Animate ( TimeSpan duration, double objective ) { Duration = duration; Objective = objective; }
+
+		public void Start () { IsAnimating = true; }
+		public void Stop () { IsAnimating = false; Position = new TimeSpan (); Animated = 0; TotalAnimated = 0; }
+		public void Pause () { IsAnimating = false; }
+
+		public void Update ( GameTime gameTime )
+		{
+			Position += gameTime.ElapsedGameTime;
+			double totalAnimated = TotalAnimated;
+			TotalAnimated = Position.TotalMilliseconds / Duration.TotalMilliseconds * Objective;
+			Animated = TotalAnimated - totalAnimated;
+
+			if ( Objective <= TotalAnimated )
+			{
+				TotalAnimated = Objective;
+				IsAnimating = false;
+			}
+
+			lastElapsed = gameTime.ElapsedGameTime;
+		}
+	}
+}
